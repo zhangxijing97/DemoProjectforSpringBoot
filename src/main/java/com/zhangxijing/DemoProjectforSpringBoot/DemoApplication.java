7e71dff072cf7250708a4337a5197dfb9485357c
Xijing Zhang <zhangxijing97@gmail.com>
2024-02-03 17:21:38 -0800

    Update

--- src/main/java/com/zhangxijing/DemoProjectforSpringBoot/DemoApplication.java
+++ src/main/java/com/zhangxijing/DemoProjectforSpringBoot/DemoApplication.java
@@ -2,6 +2,12 @@ package com.zhangxijing.DemoProjectforSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zhangxijing.DemoProjectforSpringBoot.student.Student;
+import java.util.List;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +15,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
